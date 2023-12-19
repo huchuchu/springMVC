c57efaf654835daa1eab7f009f148a7c6ddab07e
HUCHUCHU <huchuchu.yoo@gmail.com>
2023-12-19 15:15:49 +0900

    check, uncheck exception

--- springtx/src/test/java/hello/springtx/apply/InitTxTest.java
+++ springtx/src/test/java/hello/springtx/apply/InitTxTest.java
@@ -2,7 +2,13 @@ package hello.springtx.apply;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -11,19 +17,37 @@ import javax.sound.midi.Track;
 @SpringBootTest
 public class InitTxTest {
 
+    @Autowired Hello hello;
+
+    @Test
+    void go(){
+        //초기화 코드는 스프링이 초기화 시점에 호출한다.
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig{
+        @Bean
+        Hello hello(){
+            return new Hello();
+        }
+
+    }
+
     @Slf4j
     static class Hello{
 
-        @Transactional
         @PostConstruct
+        @Transactional
         public void InitV1(){
-
             boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
             log.info("Hello init @PostConstruct tx active={}", isActive);
-
         }
-        public void InitV2(){
 
+        @EventListener(ApplicationReadyEvent.class) // 스프링 컨테이너가 모두 뜨고 실행
+        @Transactional
+        public void InitV2(){
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init ApplicationReadyEvent tx active={}", isActive);
         }
 
     }
